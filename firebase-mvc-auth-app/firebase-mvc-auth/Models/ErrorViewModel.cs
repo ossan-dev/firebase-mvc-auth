532e0e6dcbd7eef3715fd80709eecb1109ebef36
Ivan Pesenti <ivan.pesenti@abenergie.it>
2021-10-21 18:13:10 +0200

    initial commit

---
+++ firebase-mvc-auth-app/firebase-mvc-auth/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace firebase_mvc_auth.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
