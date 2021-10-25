fa0836a89775e308fc18f220c955a08bd62f63f5
Ivan Pesenti <ivan.pesenti@abenergie.it>
2021-10-25 08:33:57 +0200

    changed logic to sign-in user in firebase

--- firebase-mvc-auth-app/firebase-mvc-auth/wwwroot/js/site.js
+++ firebase-mvc-auth-app/firebase-mvc-auth/wwwroot/js/site.js
@@ -1,4 +1,40 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-auth.js";
 
-// Write your JavaScript code.
+document.getElementById("login").onclick = function () { loginFunction() };
+
+function loginFunction() {
+    // Your web app's Firebase configuration
+    const firebaseConfig = {
+        apiKey: "AIzaSyDKthYav1dxl-rT6bfoInVGJg4QTkAVrBM",
+        authDomain: "test-js-authentication.firebaseapp.com",
+        projectId: "test-js-authentication",
+        storageBucket: "test-js-authentication.appspot.com",
+        messagingSenderId: "912573819501",
+        appId: "1:912573819501:web:f427c5e8742c05f3f8ad64"
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+
+    const auth = getAuth();
+
+    const username = $("#Email").val();
+    const password = $("#Pass").val();
+
+    console.log(username);
+    console.log(password);
+
+    signInWithEmailAndPassword(auth, username, password)
+        .then((userCredential) => {
+            // Signed in
+            const user = userCredential.user;
+            const jwt = user.stsTokenManager.accessToken;
+            var token = $('input[name=__RequestVerificationToken]').val();
+        })
+        .catch((error) => {
+            const errorMessage = error.message;
+            console.log("ERROR: " + errorMessage);
+        });
+}
